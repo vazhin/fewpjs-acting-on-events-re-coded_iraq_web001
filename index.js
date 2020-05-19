edd3c56a68a5282e1cd6b9fbbffb003260e1a688
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-05-19 23:12:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,8 +10,20 @@ function moveDodgerLeft() {
   }
 }
 
+
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
     moveDodgerLeft();
   }
 });
+
