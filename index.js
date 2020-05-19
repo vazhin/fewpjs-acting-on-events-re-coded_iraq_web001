fb4d1bdb57c8da2f88596ec4ec84417664f1e819
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-05-19 23:19:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function moveDodgerRight() {
   let leftNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
 
-  if (left > 0) {
+  if (left < 360) {
     dodger.style.left = `${left + 1}px`;
   }
 }
